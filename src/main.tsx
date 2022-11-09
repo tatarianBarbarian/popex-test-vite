8a75a0aa33d539cc6432944348b4e6d5b29bdea7
Felix Khafizov <f_khafizov@protonmail.com>
2022-11-09 13:46:30 +0200

    Add wagmi config and client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 import "./index.css";
 import Root, { loader as RootLoader } from "./routes/root";
 import User, { loader as UserLoader } from "./routes/user";
+
+import {
+  WagmiConfig,
+  createClient,
+  defaultChains,
+  configureChains,
+} from "wagmi";
+
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { Header } from "./Header";
+
+const alchemyKey = import.meta.env.VITE_ALCHEMY_KEY;
+const providers = alchemyKey
+  ? [alchemyProvider({ apiKey: alchemyKey }), publicProvider()]
+  : [publicProvider()];
+
+const { chains, provider, webSocketProvider } = configureChains(
+  defaultChains,
+  providers
+);
+
+const client = createClient({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  provider,
+  webSocketProvider,
+});
 
 const router = createBrowserRouter([
   {
@@ -21,7 +49,8 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <WagmiConfig client={client}>
+    <Header />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </WagmiConfig>
 );
