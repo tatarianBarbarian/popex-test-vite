9f00ee5a3b69a06e4d61e1eda73e30930b03ad6d
Felix Khafizov <f_khafizov@protonmail.com>
2022-11-09 12:12:54 +0200

    Add error page

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { Link, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error: Object = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" className="text-center">
+      <h1>Eeew!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <Link to="/" className="text-sky-700 hover:underline">
+        Go to main
+      </Link>
+    </div>
+  );
+}
