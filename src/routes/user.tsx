ab02e93b4063b0b540380a85f09772c2eda3eead
Felix Khafizov <f_khafizov@protonmail.com>
2022-11-09 12:12:45 +0200

    Add user route

---
+++ src/routes/user.tsx
@@ -0,0 +1,35 @@
+import { Link, useLoaderData } from "react-router-dom";
+import { fetchData } from "../api/fetch";
+
+export async function loader({ params }) {
+  return fetchData(`/users/${params.userId}`);
+}
+
+function User() {
+  const user: any = useLoaderData();
+  console.log(user);
+
+  return (
+    <div className="w-2/4 mx-auto">
+      <div className="p-4">
+        <Link to="/" className="text-sky-700 hover:underline">
+          Back to main
+        </Link>
+      </div>
+      <div className="text-center">
+        <img
+          src={user.avatar}
+          className="mx-auto"
+          alt={user.name + " avatar"}
+        />
+        <p className="my-5">Random pic!</p>
+        <img
+          src="https://source.unsplash.com/random"
+          alt="Absolutely random pic!"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default User;
