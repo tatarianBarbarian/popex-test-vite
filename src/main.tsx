cea5c8736c26aa3bd7e95ae414ece17fb8cb8b76
Felix Khafizov <f_khafizov@protonmail.com>
2022-11-09 12:13:07 +0200

    Build routes together

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import "./index.css";
+import Root, { loader as RootLoader } from "./routes/root";
+import User, { loader as UserLoader } from "./routes/user";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: RootLoader,
+  },
+  {
+    path: "/user/:userId",
+    element: <User />,
+    loader: UserLoader,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
